06bd57a7f7c353f7d2b5c314924c569332e2c500
saimohith71 <saimohith71@gmail.com>
2017-04-02 22:29:46 +0530

    Update views.py

--- realofficesite/viewer/views.py
+++ realofficesite/viewer/views.py
@@ -40,6 +40,46 @@ def test(request):
     conn.close()
     output = row[1]+str(row[2])
     return HttpResponse(output)
+    
+def event_exists(e_n):
+	conn = sq.connect('realoffice.db')
+    cursor=conn.execute("SELECT * FROM EVENT WHERE NAME = "+str(e_n))
+    exists = False
+    for row in cursor:
+    	exists = True
+    conn.close()
+    return exists
+    
+def venue_exists(e_v):
+	conn = sq.connect('realoffice.db')
+    cursor=conn.execute("SELECT * FROM VENUE WHERE VENUE_NAME = "+str(e_v))
+    exists = False
+    for row in cursor:
+    	exists = True
+    conn.close()
+    return exists
+    
+def venue_clash(e_v,e_s,e_e):
+
+def cap_insuf(e_v,e_c):
+	conn = sq.connect('realoffice.db')
+    cursor=conn.execute("SELECT * FROM VENUE WHERE VENUE_NAME = "+str(e_v)+" AND VENUE_CAP < "+str(e_c))
+    exists = False
+    for row in cursor:
+    	exists = True
+    conn.close()
+    return exists
+
+def no_proj(e_v,e_p):
+	if e_p==0:
+		return False
+	conn = sq.connect('realoffice.db')
+    cursor=conn.execute("SELECT * FROM VENUE WHERE VENUE_NAME = "+str(e_v)+" AND VENUE_PROJ = 0")
+    exists = False
+    for row in cursor:
+    	exists = True
+    conn.close()
+    return exists
 
 def add(request):
     data=request.body
@@ -48,19 +88,29 @@ def add(request):
     l=data.split()
     e_name=l[0]
     e_n=str(e_name).replace("b'","\"").replace("'","\"")
+    if(event_exists(e_n)):
+    	
     e_venue=l[1]
     e_v=str(e_venue).replace("b'","\"").replace("'","\"")
+    if(not venue_exists):
+    	
     e_stime=l[2]
     e_s=str(e_stime).replace("b'","\"").replace("'","\"")
     e_etime=l[3]
     e_e=str(e_etime).replace("b'","\"").replace("'","\"")
+    if(venue_clash(e_v,e_s,e_e)):
+    
     e_capacity=l[4]
     e_c=str(e_capacity).replace("b'","\"").replace("'","\"")
+    if(cap_insuf(e_v,e_c)):
+    	
     e_proj=l[5]
     e_p=str(e_proj).replace("b'","\"").replace("'","\"")
+    if(no_proj(e_v,e_p)):
+    	
     conn1 = sq.connect('realoffice.db')
     conn1.execute("INSERT INTO EVENT(NAME,TYPE,HOST1,PARTICIPANTS,VENUE,STARTTIME,ENDTIME,PROJ) \
-        VALUES("+str(e_n)+",'Meeting',1,'YOU',"+str(e_v)+","+str(e_s)+","+str(e_e)+",1)");
+        VALUES("+str(e_n)+",'Meeting',1,'YOU',"+str(e_v)+","+str(e_s)+","+str(e_e)+","+str(e_p)")");
     conn1.commit()
     conn1.close()
     return HttpResponse(data_rep)
@@ -69,6 +119,8 @@ def find(request):
     data=request.body
     print(data)
     name=str(data).replace("b'","\"").replace("'","\"")
+    if(not event_exists(name)):
+    	
     conn2 = sq.connect('realoffice.db')
     cursor=conn2.execute('SELECT VENUE FROM EVENT WHERE NAME = '+str(name) + 'AND PROJ = 1')
     for row in cursor:
@@ -77,3 +129,15 @@ def find(request):
     output=row[0]
     print(output)
     return HttpResponse(output)
+    
+def delete(request):
+	data=request.body
+    print(data)
+    name=str(data).replace("b'","\"").replace("'","\"")   
+    if(not event_exists(name)):
+    	
+    conn3 = sq.connect('realoffice.db') 
+    cursor = conn3.execute("DELETE FROM EVENT WHERE NAME = "+str(name))
+    conn3.commit()
+    conn3.close()
+    return *******
